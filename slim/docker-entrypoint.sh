7d02b400383ec2650427d7401d2a594e3e22216e
William Boman <william@redwill.se>
2016-10-17 11:58:42 +0200

    Dockerfile: use sh rather than bash

--- slim/docker-entrypoint.sh
+++ slim/docker-entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -d /home/lounge/src ]]; then
+if [ -d /home/lounge/src ]; then
     # pre 2.0.0 we ran The Lounge from /home/lounge/src/index.js.
     # This enabled users to mount a volume in that location, effectively
     # running a completely different installation of The Lounge, while keeping
@@ -8,9 +8,15 @@ if [[ -d /home/lounge/src ]]; then
     >&2 printf "%s\n\n" "The Lounge is now installed through a global npm installation. Ignoring contents in /home/lounge/src!"
 fi
 
-[[ -n $HOST ]] && CONF_OPT_HOST="--host $HOST"
-[[ -n $PORT ]] && CONF_OPT_PORT="--port $PORT"
-[[ -n $BIND ]] && CONF_OPT_BIND="--bind $BIND"
+if [ ! -z "$HOST" ]; then
+    CONF_OPT_HOST="--host $HOST"
+fi
+if [ ! -z "$PORT" ]; then
+    CONF_OPT_PORT="--port $PORT"
+fi
+if [ ! -z "$BIND" ]; then
+   CONF_OPT_BIND="--bind $BIND"
+fi
 
 lounge \
     $CONF_OPT_HOST \
