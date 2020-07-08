6a2521f162cbf1de4a49195cdc9e588201767f9e
William Boman <william@redwill.se>
2020-07-08 17:42:14 +0200

    alpine: fix bug in conditional expression (#108)

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$1" = "thelounge" -a "$(id -u)" = '0' ]; then
+if [ "$1" = "thelounge" ] && [ "$(id -u)" = '0' ]; then
     find "${THELOUNGE_HOME}" \! -user node -exec chown node '{}' +
     exec su node -c "$*"
 fi
