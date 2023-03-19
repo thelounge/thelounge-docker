de1bb013a510af0caefd7138dd676edadc7e8f65
William Boman <william@redwill.se>
2023-03-19 15:42:54 +0100

    refactor: consolidate tags in single array

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -3,27 +3,29 @@
 
 set -euo pipefail
 
-VERSION="$(git describe --exact-match --tags HEAD)"
+# VERSION="$(git describe --exact-match --tags HEAD)"
+VERSION="4.4.0-pre.1"
 TAG="$VERSION"
 MAJOR_TAG="$(sed -nre 's/^([0-9]+).*/\1/p' <<< "$VERSION")"
-EXTRA_TAGS=()
-
 DOCKERHUB_IMAGE="thelounge/thelounge"
 GITHUB_IMAGE="ghcr.io/thelounge/thelounge"
+TAGS=(
+    "--tag" "${DOCKERHUB_IMAGE}:${TAG}"
+    "--tag" "${GITHUB_IMAGE}:${TAG}"
+)
+
 
 # If not a pre-release push :latest & MAJOR_TAG
 if grep -qE "^[0-9]*\.[0-9]*\.[0-9]*$" <<< "${VERSION}"; then
-    EXTRA_TAGS+=("--tag" "${DOCKERHUB_IMAGE}:latest")
-    EXTRA_TAGS+=("--tag" "${DOCKERHUB_IMAGE}:${MAJOR_TAG}")
-    EXTRA_TAGS+=("--tag" "${GITHUB_IMAGE}:latest")
-    EXTRA_TAGS+=("--tag" "${GITHUB_IMAGE}:${MAJOR_TAG}")
+    TAGS+=("--tag" "${DOCKERHUB_IMAGE}:latest")
+    TAGS+=("--tag" "${DOCKERHUB_IMAGE}:${MAJOR_TAG}")
+    TAGS+=("--tag" "${GITHUB_IMAGE}:latest")
+    TAGS+=("--tag" "${GITHUB_IMAGE}:${MAJOR_TAG}")
 fi
 
 docker buildx build \
     --push \
     --platform "${PLATFORMS}" \
-    --tag "${DOCKERHUB_IMAGE}:${TAG}" \
-    --tag "${GITHUB_IMAGE}:${TAG}" \
-    "${EXTRA_TAGS[@]}" \
+    "${TAGS[@]}" \
     --file Dockerfile \
     .
