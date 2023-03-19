8939298b92cc6e6d336c8aa9df37e22caae4e124
William Boman <william@redwill.se>
2023-03-19 15:46:19 +0100

    fix: remove hardcoded version used for testing purposes
    Accidentally committed.

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -3,8 +3,7 @@
 
 set -euo pipefail
 
-# VERSION="$(git describe --exact-match --tags HEAD)"
-VERSION="4.4.0-pre.1"
+VERSION="$(git describe --exact-match --tags HEAD)"
 TAG="$VERSION"
 MAJOR_TAG="$(sed -nre 's/^([0-9]+).*/\1/p' <<< "$VERSION")"
 DOCKERHUB_IMAGE="thelounge/thelounge"
@@ -17,10 +16,12 @@ TAGS=(
 
 # If not a pre-release push :latest & MAJOR_TAG
 if grep -qE "^[0-9]*\.[0-9]*\.[0-9]*$" <<< "${VERSION}"; then
-    TAGS+=("--tag" "${DOCKERHUB_IMAGE}:latest")
-    TAGS+=("--tag" "${DOCKERHUB_IMAGE}:${MAJOR_TAG}")
-    TAGS+=("--tag" "${GITHUB_IMAGE}:latest")
-    TAGS+=("--tag" "${GITHUB_IMAGE}:${MAJOR_TAG}")
+    TAGS+=(
+        "--tag" "${DOCKERHUB_IMAGE}:latest"
+        "--tag" "${DOCKERHUB_IMAGE}:${MAJOR_TAG}"
+        "--tag" "${GITHUB_IMAGE}:latest"
+        "--tag" "${GITHUB_IMAGE}:${MAJOR_TAG}"
+    )
 fi
 
 docker buildx build \
