828bbcae94f0dc79460c448e4a7ec4c081adf119
William Boman <william@redwill.se>
2023-03-19 15:32:44 +0100

    ci: also publish images to ghcr.io

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -6,18 +6,24 @@ set -euo pipefail
 VERSION="$(git describe --exact-match --tags HEAD)"
 TAG="$VERSION"
 MAJOR_TAG="$(sed -nre 's/^([0-9]+).*/\1/p' <<< "$VERSION")"
-LATEST_TAG="latest"
+EXTRA_TAGS=()
 
-# If not a pre-release push LATEST_TAG & MAJOR_TAG
+DOCKERHUB_IMAGE="thelounge/thelounge"
+GITHUB_IMAGE="ghcr.io/thelounge/thelounge"
+
+# If not a pre-release push :latest & MAJOR_TAG
 if grep -qE "^[0-9]*\.[0-9]*\.[0-9]*$" <<< "${VERSION}"; then
-    EXTRA_ARG+=("--tag" "${DOCKER_REPOSITORY}:${LATEST_TAG}")
-    EXTRA_ARG+=("--tag" "${DOCKER_REPOSITORY}:${MAJOR_TAG}")
+    EXTRA_TAGS+=("--tag" "${DOCKERHUB_IMAGE}:latest")
+    EXTRA_TAGS+=("--tag" "${DOCKERHUB_IMAGE}:${MAJOR_TAG}")
+    EXTRA_TAGS+=("--tag" "${GITHUB_IMAGE}:latest")
+    EXTRA_TAGS+=("--tag" "${GITHUB_IMAGE}:${MAJOR_TAG}")
 fi
 
 docker buildx build \
     --push \
     --platform "${PLATFORMS}" \
-    --tag "${DOCKER_REPOSITORY}:${TAG}" \
-    "${EXTRA_ARG[@]}" \
+    --tag "${DOCKERHUB_IMAGE}:${TAG}" \
+    --tag "${GITHUB_IMAGE}:${TAG}" \
+    "${EXTRA_TAGS[@]}" \
     --file Dockerfile \
     .
